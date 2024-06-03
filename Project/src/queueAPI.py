772515035bc6381d66c8ac82f2b7727a6061df8e
Devinevangelista <devinevang@outlook.com>
2024-06-03 00:59:51 -0400

    Added poetry and docker, organized files

--- Project/src/queueAPI.py
+++ Project/src/queueAPI.py
@@ -66,4 +66,4 @@ api.add_resource(JobStatusResource, '/jobs/status')
 api.add_resource(JobRemoveResource, '/jobs/<string:uuid>')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
--- src/testingFlask/tester.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-BASE = "http://127.0.0.1:5000/"
-
-#send a get request to the url that is BASE + helloworld
-response = requests.get(BASE + "helloworld/tim")
-print(response.json())
